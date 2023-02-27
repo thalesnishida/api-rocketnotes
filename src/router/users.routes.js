853b1ce6fdf4d75bf4c3d3c2c999aef7bcb1fddb
Thales Nihsida <medeiros.tn@gmail.com>
2023-02-27 12:49:48 -0300

    create folder upload

--- src/router/users.routes.js
+++ src/router/users.routes.js
@@ -1,13 +1,25 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UserController = require("../controllers/UserController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRouter = Router();
+const upload = multer(uploadConfig.MULTER);
 
-const userController =  new UserController();
+const userController = new UserController();
 
 usersRouter.post("/", userController.create);
 usersRouter.put("/", ensureAuthenticated, userController.update);
+usersRouter.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  (request, response) => {
+    console.log(request.file.filename);
+    return response.json();
+  }
+);
 
 module.exports = usersRouter;
