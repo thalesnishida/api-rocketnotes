f1565f910d0020ff1df14ee5caf2aa181239d7e5
Thales Nishida <medeiros.tn@gmail.com>
2023-03-04 21:35:22 -0300

    install cors

--- src/server.js
+++ src/server.js
@@ -4,10 +4,16 @@ const express = require("express");
 
 const migration = require("./database/sqlite/migrations");
 
+const cors = require("cors");
+
 const AppError = require("./utils/AppError");
 
+const uploadConfig = require("./configs/upload");
+
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 migration();
 
@@ -15,22 +21,23 @@ const routes = require("./router");
 
 app.use(routes);
 
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
+
 app.use((error, request, response, next) => {
-  if(error instanceof AppError){
+  if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       status: "error",
-      message: error.message
-    })
+      message: error.message,
+    });
   }
 
-  console.error(error)
+  console.error(error);
 
   return response.status(500).json({
     status: "error",
-    message: "Internal server error!"
-  })
-
-})
+    message: "Internal server error!",
+  });
+});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
