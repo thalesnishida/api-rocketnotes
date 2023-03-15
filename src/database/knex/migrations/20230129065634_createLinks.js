90099c731fb78c3d58ec30713710f81c9552b7ce
Thales Nishida <medeiros.tn@gmail.com>
2023-03-15 16:37:46 -0300

    fix bug

--- src/database/knex/migrations/20230129065634_createLinks.js
+++ src/database/knex/migrations/20230129065634_createLinks.js
@@ -1,12 +1,14 @@
-exports.up = knex => knex.schema.createTable("links", table => {
-  table.increments("id");
-  table.text("url").notNullable();
+exports.up = (knex) =>
+  knex.schema.createTable("links", (table) => {
+    table.increments("id");
+    table.text("url");
 
-  table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");
-  table.timestamp("create_at").default(knex.fn.now());
+    table
+      .integer("note_id")
+      .references("id")
+      .inTable("notes")
+      .onDelete("CASCADE");
+    table.timestamp("create_at").default(knex.fn.now());
+  });
 
-});
-
-exports.down = knex => knex.schema.createTable("links");
-
-
+exports.down = (knex) => knex.schema.createTable("links");
