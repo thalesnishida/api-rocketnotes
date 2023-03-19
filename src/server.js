cac780a4e92ce1ee4f280befa2020298ec0358b2
Thales Nishida <medeiros.tn@gmail.com>
2023-03-19 13:57:08 -0300

    add envdot

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+require("dotenv/config");
 
 const express = require("express");
 
@@ -38,5 +39,5 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
